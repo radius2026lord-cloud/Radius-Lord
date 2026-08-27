0a3ea114d9f62e83f941a055ddf000dd86866e39
radius2026lord-cloud <radius2026lord@gmail.com>
2026-08-27 02:13:42 -0700

    Enable light and dark themes on signup

---
+++ src/app/(Client)/signup/layout.tsx
@@ -0,0 +1,14 @@
+import "@/app/(Client)/Dashboard/globals.css";
+import { ThemeProvider } from "next-themes";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ar" dir="rtl" suppressHydrationWarning>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
