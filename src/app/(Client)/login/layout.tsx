5f9db063fbc1c48061c2d73b9204f9fb27f25853
radius2026lord-cloud <radius2026lord@gmail.com>
2026-08-27 02:12:35 -0700

    Enable light and dark themes on login

--- src/app/(Client)/login/layout.tsx
+++ src/app/(Client)/login/layout.tsx
@@ -1,9 +1,14 @@
-import "@/app/(Client)/Dashboard/globals.css"
+import "@/app/(Client)/Dashboard/globals.css";
+import { ThemeProvider } from "next-themes";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ar" dir="rtl" suppressHydrationWarning>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
