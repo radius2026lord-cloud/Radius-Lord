755af3f58c864da3d503f21f5380dffae6980d69
radius2026lord-cloud <radius2026lord@gmail.com>
2026-09-15 14:20:00 -0700

    Remove obsolete login success overlay wrapper

--- src/app/(Client)/login/page.tsx
+++ src/app/(Client)/login/page.tsx
@@ -1,10 +1,5 @@
 import LordAuth from "@/components/auth/lord-auth";
-import styles from "./SuccessOverlay.module.css";
 
 export default function LoginPage() {
-  return (
-    <div className={styles.scope}>
-      <LordAuth mode="login" />
-    </div>
-  );
+  return <LordAuth mode="login" />;
 }
