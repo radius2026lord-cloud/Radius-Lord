bdc25f483637b58c3020fae18b3b984caa2b35cf
radius2026lord-cloud <radius2026lord@gmail.com>
2026-09-16 04:23:51 -0700

    Move signup stepper into active signup view

--- src/app/(Client)/signup/page.tsx
+++ src/app/(Client)/signup/page.tsx
@@ -5,14 +5,6 @@ export default function SignupPage() {
   return (
     <div className="signup-page">
       <LordAuth mode="signup" />
-
-      <div className="signup-stepper" dir="rtl" aria-label="مراحل إنشاء الحساب">
-        <span className="signup-stepper__step signup-stepper__step--one">1</span>
-        <span className="signup-stepper__track" aria-hidden="true">
-          <span className="signup-stepper__progress" />
-        </span>
-        <span className="signup-stepper__step signup-stepper__step--two">2</span>
-      </div>
     </div>
   );
 }
