fabbe9a753be4c0d7ec99d6ab1d9c6c0559da72b
radius2026lord-cloud <radius2026lord@gmail.com>
2026-09-20 01:08:37 -0700

    Remove obsolete signup stepper wrapper

--- src/app/(Client)/signup/page.tsx
+++ src/app/(Client)/signup/page.tsx
@@ -1,12 +1,5 @@
-import "./signup-step-polish.css";
 import LordAuth from "@/components/auth/lord-auth";
-import { SignupStepper } from "@/components/auth/signup-stepper";
 
 export default function SignupPage() {
-  return (
-    <div className="signup-page">
-      <LordAuth mode="signup" />
-      <SignupStepper />
-    </div>
-  );
+  return <LordAuth mode="signup" />;
 }
