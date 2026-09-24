1450979f303223b4a0dfb979915ff4bee5b39528
radius2026lord-cloud <radius2026lord@gmail.com>
2026-09-23 17:08:19 -0700

    Register payment plans admin API

--- src/app/backend/routes/index.ts
+++ src/app/backend/routes/index.ts
@@ -5,6 +5,7 @@ import authRoutes from './auth/auth.routes'; // Login
 import authLogoutRoutes from './auth/logout.route'; //Logout
 import adminCustomersRoutes from './admin/customers.routes';
 import adminAuditRoutes from './admin/audit.routes';
+import adminPaymentPlansRoutes from './admin/payment-plans.routes';
 
 const router = Router();
 
@@ -14,5 +15,6 @@ router.use('/auth', authRoutes); // Login
 router.use('/auth', authLogoutRoutes);
 router.use('/admin/customers', adminCustomersRoutes);
 router.use('/admin/audit-logs', adminAuditRoutes);
+router.use('/admin/payment-plans', adminPaymentPlansRoutes);
 //Logout
 export default router;
